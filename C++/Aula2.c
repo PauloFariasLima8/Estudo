d3576e6a91ec21dfb0409a10cb11948be9ed123c
PauloFariasLima8 <paulofariaslima8@gmail.com>
2025-01-27 19:38:42 +0000

    confihurando os SDK

---
+++ C++/Aula2.c
@@ -0,0 +1,3 @@
+int idade =30; //para numeros inteiros
+float altura= 1,75; //para numeros decimais
+char inicial= 'A'; //para caracteres
---
+++ Embarcados/OrganizaçãoSDK.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include "pico/stdlib.h"
+const unit led_pin_red = 12;
+int min() {
+    uint a =100;
+    
+}
